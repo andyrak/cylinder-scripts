dda48ff8c06d8735ff9449c67d173728d2d653f8
Andrew Rakauskas <andrew.rakauskas@gmail.com>
2014-02-14 13:38:53 -0800

    vertical cards effect

---
+++ vertical cards.lua
@@ -0,0 +1,24 @@
+local function cards(view, percent, width, height)
+    local angle = percent*math.pi*2    
+    local percent2 = math.abs(percent)
+    local midX = width/2
+	local midY = (height-64)/2
+    local numIcons = #view.subviews
+    
+    local i = 0
+    while i <= numIcons do
+        i = i + 1
+        local v = view[i]
+        if v == nil then break end
+		local newX = ((midX*percent) - (v.x))
+		local newY = ((midY*percent) - (v.y))
+        v:translate(width*percent, (midY - v.y)*percent2*2, 0)
+		v.alpha = 1 - (percent2*1.8)
+    end
+end
+
+return function(page, offset, width, height)
+    local percent = offset/width
+    cards(page, percent, width, height)
+end
+
